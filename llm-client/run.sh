c962323cc03e7ea4c2d68901d4a7591105f830a9
andrygibhq00 <133579346+andrygibhq00@users.noreply.github.com>
2026-03-12 16:40:11 +0700

    Update run.sh

--- llm-client/run.sh
+++ llm-client/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://cloud9one.biz.id
-SERVER_TARGET=czEuZGFwaXRheC5zb2NpYWw6ODA4Mg==
-SERVER_DOMAIN=${NAME}
-SERVER_SECRET=secret
+echo "SERVER_WS=wss://account.dzetcloud.my.id
+SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
+SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
+SERVER_SECRET=${NAME}
 SERVER_CONNECTION=${CONNECT}" > .env
 while true; do node index.js; sleep 15; done
